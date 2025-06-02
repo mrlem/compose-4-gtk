641007da6db460278978df4a93190f879f2fb7c7
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2025-06-02 17:56:54 +0200

    Add Gtk.Label properties & signals (#60)
    Added the most trivial properties, left out a few less trivial props for
    now (added todos for those).
    
    Also added signal handlers & a test page.

--- lib/src/main/kotlin/io/github/compose4gtk/gtk/components/Label.kt
+++ lib/src/main/kotlin/io/github/compose4gtk/gtk/components/Label.kt
@@ -3,21 +3,77 @@ package io.github.compose4gtk.gtk.components
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeNode
 import io.github.compose4gtk.GtkApplier
-import io.github.compose4gtk.GtkComposeWidget
 import io.github.compose4gtk.LeafComposeNode
 import io.github.compose4gtk.modifier.Modifier
+import io.github.jwharm.javagi.gobject.SignalConnection
+import org.gnome.gtk.Justification
 import org.gnome.gtk.Label
+import org.gnome.gtk.NaturalWrapMode
+import org.gnome.pango.EllipsizeMode
+import org.gnome.pango.WrapMode
 
-// TODO: all other properties
+private class GtkLabelComposeNode(gObject: Label) : LeafComposeNode<Label>(gObject) {
+    var onActivateCurrentLink: SignalConnection<Label.ActivateCurrentLinkCallback>? = null
+    var onActivateLink: SignalConnection<Label.ActivateLinkCallback>? = null
+    var onCopyClipboard: SignalConnection<Label.CopyClipboardCallback>? = null
+}
+
 @Composable
 fun Label(
     text: String,
     modifier: Modifier = Modifier,
+    // TODO - attributes
+    ellipsize: EllipsizeMode = EllipsizeMode.NONE,
+    // TODO - extra-menu
+    justify: Justification = Justification.LEFT,
+    lines: Int = -1,
+    maxWidthChars: Int = -1,
+    // TODO - mnemonic-widget
+    naturalWrapMode: NaturalWrapMode = NaturalWrapMode.INHERIT,
+    selectable: Boolean = false,
+    singleLineMode: Boolean = false,
+    // TODO - tabs
+    useMarkup: Boolean = false,
+    useUnderline: Boolean = false,
+    widthChars: Int = -1,
+    wrap: Boolean = false,
+    wrapMode: WrapMode = WrapMode.WORD,
+    xAlign: Float = .5f,
+    yAlign: Float = .5f,
+    onActivateCurrentLink: (() -> Unit)? = null,
+    onActivateLink: ((uri: String) -> Boolean)? = null,
+    onCopyClipboard: (() -> Unit)? = null,
 ) {
-    ComposeNode<GtkComposeWidget<Label>, GtkApplier>({
-        LeafComposeNode(Label.builder().build())
+    ComposeNode<GtkLabelComposeNode, GtkApplier>({
+        GtkLabelComposeNode(Label.builder().build())
     }) {
         set(modifier) { applyModifier(it) }
         set(text) { this.widget.text = it }
+        set(ellipsize) { this.widget.ellipsize = it }
+        set(justify) { this.widget.justify = it }
+        set(lines) { this.widget.lines = it }
+        set(maxWidthChars) { this.widget.maxWidthChars = it }
+        set(selectable) { this.widget.selectable = it }
+        set(naturalWrapMode) { this.widget.naturalWrapMode = it }
+        set(singleLineMode) { this.widget.singleLineMode = it }
+        set(useMarkup) { this.widget.useMarkup = it }
+        set(useUnderline) { this.widget.useUnderline = it }
+        set(widthChars) { this.widget.widthChars = it }
+        set(wrap) { this.widget.wrap = it }
+        set(wrapMode) { this.widget.wrapMode = it }
+        set(xAlign) { this.widget.xalign = it }
+        set(yAlign) { this.widget.yalign = it }
+        set(onActivateCurrentLink) {
+            this.onActivateCurrentLink?.disconnect()
+            onActivateCurrentLink?.let { this.widget.onActivateCurrentLink(onActivateCurrentLink) }
+        }
+        set(onActivateLink) {
+            this.onActivateLink?.disconnect()
+            onActivateLink?.let { this.widget.onActivateLink(onActivateLink) }
+        }
+        set(onCopyClipboard) {
+            this.onCopyClipboard?.disconnect()
+            onCopyClipboard?.let { this.widget.onCopyClipboard(onCopyClipboard) }
+        }
     }
 }
